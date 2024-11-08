95ea56dc45a112eecd9a203e84d6cb58205c8523
wpineda <wpineda@cultura.gob.sv>
2024-11-07 23:28:00 -0600

    [New]: map & confirm sections

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -117,6 +117,49 @@
             </div>
         </div>
         {{-- counter --}}
+
+        {{-- location --}}
+
+        <div class="location">
+            <div class="location-left">
+                <div class="location-item">
+                    <img src="img/map.png" class="" alt="">
+                    <p class="epilogue-location">Palo Alto, Lago de Coatepeque</p>
+                </div>
+                <div class="location-item">
+                    <div class="location-item-left">
+                        <img src="img/suit.png" class="" alt="">
+                    </div>
+                    <div class="location-item-right">
+                        <div>
+                            <p class="epilogue-location"><b>Caballeros:</b> Traje formal o business casual</p>
+                        </div>
+                        <div>
+                            <p class="epilogue-location"><b>Señoritas:</b> Vestidos (no color blanco)</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="location-item">
+                     <img src="img/clock.png" class="" alt="">
+                    <p class="epilogue-location">4:00 p.m. - hasta más tarde</p>
+                </div>
+            </div>
+            <div class="location-right">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1936.5707290782816!2d-89.5320410138011!3d13.890488657564635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f62ddd768c832ad%3A0xc791d686a678f0f9!2sQuinta%20palo%20alto!5e0!3m2!1ses!2ssv!4v1731041167227!5m2!1ses!2ssv" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+        {{-- location --}}
+
+        {{-- confirm --}}
+        <div class="confirm">
+            <div class="confirm-left">
+                <p class="epilogue-confirm">Confirmar / Mesas</p>
+            </div>
+            <div class="confirm-right">
+                <p class="epilogue-confirm-p">¡Verifica tu ubicación y acomódate como debe de ser! Gracias por ser parte de nuestro evento.</p>
+            </div>
+        </div>
+        {{-- confirm --}}
         
     </div>
         
